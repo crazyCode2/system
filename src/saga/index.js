855c0070c9eb4e6a988c8bc93bbd8d38a311d34c
crazyCode2 <1178770858@qq.com>
2018-05-04 23:03:48 +0800

    start a new project

---
+++ src/saga/index.js
@@ -0,0 +1,28 @@
+/**
+ * saga 入口文件
+ * 异步流 redux-saga
+ */
+// 引入 redux-saga
+import {
+  put,
+  takeEvery,
+  fork,
+  all
+} from 'redux-saga/effects';
+// 登录页 模拟数据
+import loginSaga from './loginSaga';
+// 用户信息
+import userSaga from './userSaga';
+// 通知栏数据
+import global from './globalSaga';
+
+// 所有的saga的入口配置文件
+const config = [
+  fork(loginSaga),
+  fork(userSaga),
+  fork(global),
+];
+
+export default function* rootSaga(){
+  yield all(config)
+}
